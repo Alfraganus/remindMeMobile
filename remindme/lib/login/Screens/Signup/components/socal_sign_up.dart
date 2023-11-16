379bcd75567dfe8e39dc5c98eb422fa38ba5f3bb
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-11-16 22:28:58 +0500

    Initial commit

---
+++ remindme/lib/login/Screens/Signup/components/socal_sign_up.dart
@@ -0,0 +1,36 @@
+// import 'package:flutter/material.dart';
+
+// import '../../../screens/Signup/components/or_divider.dart';
+// import '../../../screens/Signup/components/social_icon.dart';
+
+// class SocalSignUp extends StatelessWidget {
+//   const SocalSignUp({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         const OrDivider(),
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             SocalIcon(
+//               iconSrc: "icons/facebook.svg",
+//               press: () {},
+//             ),
+//             SocalIcon(
+//               iconSrc: "icons/twitter.svg",
+//               press: () {},
+//             ),
+//             SocalIcon(
+//               iconSrc: "icons/google-plus.svg",
+//               press: () {},
+//             ),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
